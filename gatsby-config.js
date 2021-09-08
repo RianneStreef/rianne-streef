b06008528e27d52a719105f536e20df5252fd3c0
RianneStreef <riannestreef@gmail.com>
2021-09-08 10:22:30 +0200

    brave verifivation added

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,11 +17,24 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
-        fonts: [
-          `Montserrat`,
-          `source sans pro\:300,400,400i,`, // you can also specify font weights and styles
-        ],
+        fonts: [`Montserrat`, `source sans pro\:300,400,400i,`],
         display: "swap",
+      },
+    },
+    {
+      resolve: "gatsby-plugin-mailchimp",
+      options: {
+        endpoint:
+          "https://riannestreef.us6.list-manage.com/subscribe/post?u=afbb7e209cb6169f77f6f8d99&amp;id=df7adc3a54", // string; add your MC list endpoint here; see instructions below
+        timeout: 3500, // number; the amount of time, in milliseconds, that you want to allow mailchimp to respond to your request before timing out. defaults to 3500
+      },
+    },
+    {
+      resolve: "gatsby-plugin-verify-brave",
+      options: {
+        token:
+          "7863c4ef95dc9b74f82a04a7700d48f98d09a049b46683bb4b4ca66c0331c3e3",
+        domain: "riannestreef.com",
       },
     },
   ],
